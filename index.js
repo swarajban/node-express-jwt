f6b8f5cdf1a42efd0c38271b6ae3f97284479fa3
Swaraj Banerjee <swarajban@gmail.com>
2016-01-08 20:51:25 -0800

    Generated first JWTs! Added GET /jwt-sso route that renders login page, and POST /jwt-sso route that generates JWT for logged in user

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var bodyParser = require('body-parser');
 var passport = require('passport');
 var LocalStrategy = require('passport-local');
 
+var uuid = require('node-uuid');
+var jwt = require('jsonwebtoken');
+
 // 21st century database
 var users = require('./users.json');
 
@@ -14,6 +17,10 @@ var app = express();
 app.set('port', process.env.EXPRESS_PORT || 3000);
 app.set('views', './views');
 app.set('view engine', 'jade');
+
+app.set('my-domain', process.env.DOMAIN || 'example.com');
+app.set('jwt-secret', process.env.JWT_SECRET || 'pc-load-letter');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
@@ -90,6 +97,50 @@ app.post('/sso',
     successRedirect: '/',
     failureRedirect: '/sso'
   })
+);
+
+app.get('/jwt-sso',
+  function (req, res) {
+    res.render('login', {
+      loginRoute: '/jwt-sso'
+    });
+  }
+);
+
+app.post('/jwt-sso',
+  function (req, res, next) {
+    passport.authenticate('local',
+      function (err, user, info) {
+        if (err) {
+          return next(err);
+        }
+
+        if (! user) { // failed to authenticate, try logging in again
+          return res.redirect('/jwt-sso');
+        }
+
+        var jwtPayload = {
+          email: user.email
+          // Can add developer keys here
+        };
+
+        var jwtSecret = app.get('jwt-secret');
+
+        var jwtOptions = {
+          algorithm: 'HS256',
+          jwtid: uuid.v4(),
+          issuer: app.get('my-domain')
+        };
+
+        var token = jwt.sign(jwtPayload, jwtSecret, jwtOptions);
+        console.log('Created JWT: ' + token);
+
+        res.render('message', {
+          message: 'JWT: ' + token
+        });
+      }
+    )(req, res, next);
+  }
 );
 
 
